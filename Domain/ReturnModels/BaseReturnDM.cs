2f588b932ca6f81a9b9d7f9f011dfde8cddf4ca1
Ian McNeil (Contractor) <Ian.McNeilContractor@publix.com>
2025-04-29 13:19:50 -0400

    Return Model with Base return model

---
+++ Domain/ReturnModels/BaseReturnDM.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.ReturnModels
+{
+    public class BaseReturnDM
+    {
+        public string Process { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public int SuccessCount { get; set; } = 0;
+        public List<Exception> Exceptions { get; set; } = new List<Exception>();
+    }
+}
---
+++ Domain/ReturnModels/GenericReturnDM.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Domain.ReturnModels
+{
+    public class GenericReturnDM : BaseReturnDM
+    {
+        public GenericDM? GenericReturn { get; set; }
+
+        public List<GenericDM>? GenericListReturn { get; set; } = new List<GenericDM>();
+    }
+}
